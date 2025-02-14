0bde67ba18ad4ea7752be4d5be86244c171c8872
luizobara <luizobara@gmail.com>
2025-02-14 10:54:19 -0300

    Alterando Copyright

--- components/copyright.tsx
+++ components/copyright.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 export default function Copyright() {
     return(
         <div className="w-full text-center text-slate-400">
-            <h5 className="font-semibold">© 2024 Brixton. Todos os direitos reservados.</h5>
-            <h6 className="uppercase text-sm font-bold">Desenvolvido por <Link href="https://devfast.com.br" target="blank"><span className="text-blue-600 font-semibold">Devfast</span></Link></h6>
+            <h5 className="font-semibold">© 2025 Brixton. Todos os direitos reservados.</h5>
+            <h6 className="uppercase text-sm font-bold">Desenvolvido por <Link href="https://www.linkedin.com/in/luiz-obara-544945218/" target="blank"><span className="text-blue-600 font-semibold">LuizObara</span></Link></h6>
         </div>
     );
 }
